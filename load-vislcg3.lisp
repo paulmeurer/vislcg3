bf97450a26b083d4104ef9583c49009e2f23dbc5
Paul Meurer <paul.meurer@uib.no>
2023-03-17 11:38:04 +0000

    remove dependency on cl-fst

--- load-vislcg3.lisp
+++ load-vislcg3.lisp
@@ -4,12 +4,7 @@
 ;; adapt to your environment
 
 (define-foreign-library libcg3
-  (:darwin
-   #+X86-64
-   (:default "/usr/local/lib/libcg3"))
-  (:unix
-   #+X86-64
-   (:default "/usr/local/lib/libcg3")))
+    (:default "/usr/lib64/libcg3"))
 
 (use-foreign-library libcg3)
 
